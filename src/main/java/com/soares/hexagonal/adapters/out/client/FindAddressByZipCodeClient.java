08d72449c11179e0c78a8491574d8036a592667a
Rubens <rubenssoares95@gmail.com>
2022-11-08 23:21:12 -0300

    Created Feign Client to find Address

---
+++ src/main/java/com/soares/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,17 @@
+package com.soares.hexagonal.adapters.out.client;
+
+import com.soares.hexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "FindAddressByZipCodeClient",
+        url = "${soares.client.address.url}"
+)
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/{zipCode}")
+    AddressResponse find(@PathVariable("zipCode") String zipCode);
+
+}
---
+++ src/main/java/com/soares/hexagonal/adapters/out/client/response/AddressResponse.java
@@ -0,0 +1,14 @@
+package com.soares.hexagonal.adapters.out.client.response;
+
+import lombok.Data;
+
+@Data
+public class AddressResponse {
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+}
